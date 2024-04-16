571df4ea7c8eceaea2e05f5320f44912922ba93e
Rulu <79538610+hafnium1923@users.noreply.github.com>
2024-04-16 14:39:44 +0900

    refactor: router 파일 분리 (#16)

--- src/index.tsx
+++ src/index.tsx
@@ -3,22 +3,10 @@ import { createRoot } from 'react-dom/client';
 import { Global, ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { GlobalReset } from '~/styles/GlobalReset';
-import App from '~/App';
 import { Theme } from '~/styles/Theme';
 import { serviceWorker } from '~/server/browser';
 import { ToastProvider } from '~/components/common/Toast/ToastContext';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { ROUTES } from '~/constants/routes';
-import LandingPage from '~/pages/LandingPage/LandingPage';
-import StartPage from '~/pages/StartPage/StartPage';
-import JoinPage from '~/pages/JoinPage/JoinPage';
-import CreatePage from '~/pages/CreatePage/CreatePage';
-import NotFoundPage from '~/pages/NotFoundPage/NotFoundPage';
-import LoginPage from '~/pages/LoginPage/LoginPage';
-import ProtectRoute from '~/components/common/ProtectRoute/ProtectRoute';
-import DashBoardPage from '~/pages/DashBoardPage/DashBoardPage';
-import ProjectHolder from '~/pages/ProjectHolder/ProjectHolder';
-import ProtectProjectRoute from '~/components/common/ProtectProjectRoute/ProtectProjectRoute';
+import AppRouter from '~/router/AppRouter';
 
 if (process.env.WORKER === 'on') {
   serviceWorker.start({ onUnhandledRequest: 'bypass' });
@@ -36,33 +24,6 @@ const queryClient = new QueryClient({
   },
 });
 
-const router = createBrowserRouter([
-  {
-    path: ROUTES.ROOT,
-    element: <App />,
-    errorElement: <NotFoundPage />,
-    children: [
-      { path: '', element: <LandingPage /> },
-      { path: ROUTES.LOGIN, element: <LoginPage /> },
-      {
-        element: <ProtectRoute />,
-        children: [
-          {
-            element: <ProtectProjectRoute />,
-            children: [
-              { path: ROUTES.START, element: <StartPage /> },
-              { path: ROUTES.CREATE, element: <CreatePage /> },
-              { path: ROUTES.JOIN, element: <JoinPage /> },
-            ],
-          },
-          { path: ROUTES.PROJECT_HOLDER, element: <ProjectHolder /> },
-          { path: ROUTES.DASH_BOARD, element: <DashBoardPage /> },
-        ],
-      },
-    ],
-  },
-]);
-
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
@@ -71,7 +32,7 @@ root.render(
       <ThemeProvider theme={Theme}>
         <Global styles={GlobalReset} />
         <ToastProvider>
-          <RouterProvider router={router} />
+          <AppRouter />
         </ToastProvider>
       </ThemeProvider>
     </QueryClientProvider>
---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,46 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import App from '~/App';
+import ProtectProjectRoute from '~/components/common/ProtectProjectRoute/ProtectProjectRoute';
+import ProtectRoute from '~/components/common/ProtectRoute/ProtectRoute';
+import { ROUTES } from '~/constants/routes';
+import CreatePage from '~/pages/CreatePage/CreatePage';
+import DashBoardPage from '~/pages/DashBoardPage/DashBoardPage';
+import JoinPage from '~/pages/JoinPage/JoinPage';
+import LandingPage from '~/pages/LandingPage/LandingPage';
+import LoginPage from '~/pages/LoginPage/LoginPage';
+import NotFoundPage from '~/pages/NotFoundPage/NotFoundPage';
+import ProjectHolder from '~/pages/ProjectHolder/ProjectHolder';
+import StartPage from '~/pages/StartPage/StartPage';
+
+const AppRouter = () => {
+  const router = createBrowserRouter([
+    {
+      path: ROUTES.ROOT,
+      element: <App />,
+      errorElement: <NotFoundPage />,
+      children: [
+        { path: '', element: <LandingPage /> },
+        { path: ROUTES.LOGIN, element: <LoginPage /> },
+        {
+          element: <ProtectRoute />,
+          children: [
+            {
+              element: <ProtectProjectRoute />,
+              children: [
+                { path: ROUTES.START, element: <StartPage /> },
+                { path: ROUTES.CREATE, element: <CreatePage /> },
+                { path: ROUTES.JOIN, element: <JoinPage /> },
+              ],
+            },
+            { path: ROUTES.PROJECT_HOLDER, element: <ProjectHolder /> },
+            { path: ROUTES.DASH_BOARD, element: <DashBoardPage /> },
+          ],
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default AppRouter;
